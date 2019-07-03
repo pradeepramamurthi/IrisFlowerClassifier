e6500ddc76c80db9bc62f1a0b966b0e4dd7e41c7
Pradeep Ramamurthy <pradeepramamurthi@yahoo.com>
2019-07-03 16:46:17 +0100

    Modified DFCreator file to handle bad data while creating dataframe and added additional test cases.

--- src/main/scala/com/testkraft/classifier/DFCreator.scala
+++ src/main/scala/com/testkraft/classifier/DFCreator.scala
@@ -12,25 +12,31 @@ object DFCreator extends IrisWrapper {
 
   //create a DF by taking in the path of the Iris dataset
   def buildDF(dataset: String): DataFrame = {
-    spark.read.option("inferSchema",true).option("header",true).format("csv").load(dataset).toDF()
+    val resultbuildDF = spark.read.option("inferSchema",true).option("header",true).format("csv").load(dataset).toDF()
+    resultbuildDF
   }
 
   //create a DF by taking in the path of the Iris dataset
   def buildTrainDF(dataset: String): DataFrame = {
 
-    spark.read.option("header",true).schema(Iris.mySchema).format("csv").load(dataset).toDF()
+    val TrainDF = spark.read.option("header",true).schema(Iris.mySchema).format("csv").load(dataset).toDF()
+    val resultTrainDF = TrainDF.na.fill(0.0).na.replace(Seq("SepalLength","SepalWidth","PetalLength","PetalWidth","Species"), Map("" -> "0.0"))
+    resultTrainDF
+
 
   }
 
   //return the assembled DF with additional features column (assemble only the feature set of dataframe )
   def assembleDF(dataframe: DataFrame): DataFrame = {
-    Iris.assembler.transform(dataframe)
+    val resultassembledDF = Iris.assembler.transform(dataframe)
+    resultassembledDF
   }
 
 
   //Return DF with Species column Indexed.Note: StringIndexer encodes a string column of labels to a column of label indices. The indices are in [0, numLabels), ordered by label frequencies, so the most frequent label gets index 0.
   def indexedDF(dataframe: DataFrame): DataFrame = {
-    Iris.stringIndexer.fit(dataframe).transform(dataframe)
+    val resultindexedDF = Iris.stringIndexer.fit(dataframe).transform(dataframe)
+    resultindexedDF
   }
 
 }
--- src/test/scala/IrisClassificationTests.scala
+++ src/test/scala/IrisClassificationTests.scala
@@ -3,15 +3,61 @@ import org.scalatest._
 
 class IrisClassificationTests extends FlatSpec with Matchers with IrisWrapper {
 
-  val test1inputdata: String = getClass.getClassLoader.getResource("testForIrisSentosa.csv").toString
+  val test1inputdata: String = getClass.getClassLoader.getResource("testForIrisSetosa.csv").toString
+  val test2inputdata: String = getClass.getClassLoader.getResource("testForIrisVersicolor.csv").toString
+  val test3inputdata: String = getClass.getClassLoader.getResource("testForIrisVirginica.csv").toString
+  val test4inputdata: String = getClass.getClassLoader.getResource("testZeros.csv").toString
+  val test5inputdata: String = getClass.getClassLoader.getResource("testEmptyRecords.csv").toString
+  val test6inputdata: String = getClass.getClassLoader.getResource("testExtraRecord.csv").toString
+
 
   import spark.implicits._
 
-"A testForIrisSentosa.csv" should "evaluate to Iris-sentosa" in {
+"A testForIrisSetosa.csv" should "evaluate to Iris-sentosa" in {
 
-   assert(Predict.predictInputData(test1inputdata).select("namedPrediction").as[String].collect()(0) === "Iris-Setosa")
+    //use of should be from matchers
+    Predict.predictInputData(test1inputdata).select("namedPrediction").as[String].collect()(0) should be ("Iris-Setosa")
 
 
-}
+  }
+
+  "A testForIrisVersicolor.csv" should "evaluate to Iris-versicolor" in {
+    //use of assert
+    assert(Predict.predictInputData(test2inputdata).select("namedPrediction").as[String].collect()(0) === "Iris-Versicolor")
+
+
+  }
+
+
+  "A testForIrisVirginica.csv" should "evaluate to Iris-Virginica" in {
+
+    assert(Predict.predictInputData(test3inputdata).select("namedPrediction").as[String].collect()(0) === "Iris-Virginica")
+
+
+  }
+
+  "A testZeros.csv" should "evaluate to Iris-Setosa" in {
+
+    assert(Predict.predictInputData(test4inputdata).select("namedPrediction").as[String].collect()(0) === "Iris-Setosa")
+
+
+  }
+
+
+  "A testEmptyRecords.csv" should "evaluate to ???" in {
+
+    //Checks if null and empty records are handled.
+    assert(Predict.predictInputData(test5inputdata).select("namedPrediction").as[String].collect()(0) === "Iris-Setosa")
+
+
+  }
+
+  "A testExtraRecords.csv" should "evaluate to ???" in {
+
+    //Checks behaviour if there is an extra record
+    assert(Predict.predictInputData(test6inputdata).select("namedPrediction").as[String].collect()(0) === "Iris-Setosa")
+
+
+  }
 
 }
